45364d6d99a516e87f03342c9eb355a64a0d5c77
tpaskhalis <tpaskhalis@gmail.com>
2014-06-30 08:49:29 +0100

    Added geocoding, gender estimation and logit model

--- dataanalysis.R
+++ dataanalysis.R
@@ -2,9 +2,11 @@
 setwd("/media/tom/New Volume/Dropbox/Git/MY499")
 # setwd("D:/Dropbox/Git/MY499")
 
-library(zoo)
-library(Amelia)
-library(mice)
+##########################################################################
+#Plotting timeseries for Twitter and YouGov data
+##########################################################################
+require(zoo)
+require(Amelia)
 
 # Twitter data
 twitter <- read.csv("data/followers_count.csv")
@@ -105,3 +107,118 @@ par(mar=c(0, 0, 0, 0))
 plot.new()
 legend("center",legend=c("UKIP(T)","UKIP(Y)", "Cons(T)","Cons(Y)", "Lab(T)","Lab(Y)", "LibDem(T)", "LibDem(Y)"), 
        lwd=2, lty = c(1,2,1,2,1,2,1,2), col=colours, ncol = 4, bty ="n")
+
+##########################################################################
+#Plotting maps for Twitter
+##########################################################################
+require(maptools)
+require(rgdal)
+require(ggplot2)
+require(gridExtra)
+require(plyr)
+
+europe <- readOGR(dsn = "./data/map", layer = "european_region_region")
+proj4string(europe) <- CRS("+init=epsg:27700")
+europe.wgs84 <- spTransform(europe, CRS("+init=epsg:4326"))
+
+followers <- read.csv("./data/ukip_geocoded.csv")
+followers <- followers[followers$bnp == 0 & followers$libdem == 0 & followers$cons == 0 & followers$lab == 0,]
+location <- followers[,c(17,18)]
+points <- location[complete.cases(location),]
+coordinates(points) <- ~lon + lat
+proj4string(points) <- CRS("+init=epsg:4326")
+
+overlaid <- over(points, europe.wgs84)
+countfolls <- na.omit(count(overlaid, "NAME"))
+countfolls$freq <- countfolls$freq/sum(countfolls$freq)
+
+respondents <- read.csv("./data/ukip_bes.csv")
+countres <- count(respondents, "q1")
+countres$freq <- countres$freq/sum(countres$freq)
+levels(countres$q1) <- c("Eastern Euro Region", "East Midlands Euro Region", "London Euro Region",
+                         "North East Euro Region", "North West Euro Region", "Scotland Euro Region",
+                         "South East Euro Region", "South West Euro Region", "Wales Euro Region",
+                         "West Midlands Euro Region", "Yorkshire and the Humber Euro Region", 
+                         "Yorkshire and the Humber Euro Region")
+countres$freq[11] <- sum(countres$freq[c(11,12)])
+countres <- countres[-12,]
+
+europe.f <- fortify(europe, region = "NAME")
+europe.f1 <- merge(europe.f, countres, by.x = "id", by.y = "q1")
+
+europe.f2 <- merge(europe.f, countfolls, by.x = "id", by.y = "NAME")
+
+Map1 <- ggplot(europe.f1, aes(long, lat, group = group, fill = freq)) +
+  geom_polygon() + coord_equal() + theme(axis.text = element_blank(),
+                                         axis.ticks = element_blank(),
+                                         axis.line = element_blank(),
+                                         panel.background = element_blank(),
+                                         panel.grid = element_blank()) +
+  scale_fill_gradient(low = "plum1", high = "purple4") +
+  labs(x = NULL, y = NULL, fill = "UKIP %") +
+  ggtitle("UKIP Respondents Distribution")
+
+Map2 <- ggplot(europe.f2, aes(long, lat, group = group, fill = freq)) +
+  geom_polygon() + coord_equal() + theme(axis.text = element_blank(),
+                                         axis.ticks = element_blank(),
+                                         axis.line = element_blank(),
+                                         panel.background = element_blank(),
+                                         panel.grid = element_blank()) +
+  scale_fill_gradient(low = "plum1", high = "purple4") +
+  labs(x = NULL, y = NULL, fill = "UKIP %") +
+  ggtitle("UKIP Followers Distribution")
+grid.arrange(Map1, Map2, ncol = 2)
+
+##########################################################################
+#Geocoding Twitter user location
+##########################################################################
+
+require(ggmap)
+
+metadata_uk <- read.csv("data/metadata_uk.csv", na.strings = "", stringsAsFactors = FALSE)
+geocoded <- lapply(metadata_uk[1:2500,'location'], geocode)
+metadata_uk[1:2500,'lon'] <- sapply(geocoded, function(x) {as.numeric(x[1])})
+metadata_uk[1:2500,'lat'] <- sapply(geocoded, function(x) {as.numeric(x[2])})
+write.csv(metadata_uk, file = "data/metadata_uk_geocoded.csv", row.names = FALSE)
+geocodeQueryCheck()
+
+metadata_uk <- read.csv("data/metadata_uk_geocoded.csv", na.strings = "", stringsAsFactors = FALSE)
+geocoded <- lapply(metadata_uk[6001:nrow(metadata_uk),'location'], geocode)
+metadata_uk[6001:nrow(metadata_uk),'lon'] <- sapply(geocoded, function(x) {as.numeric(x[1])})
+metadata_uk[6001:nrow(metadata_uk),'lat'] <- sapply(geocoded, function(x) {as.numeric(x[2])})
+write.csv(metadata_uk, file = "data/metadata_uk_geocoded.csv", row.names = FALSE)
+
+##########################################################################
+#Estimating probability of gender
+##########################################################################
+
+require(gender)
+
+metadata <- read.csv("data/followers_metadata_22052014.csv", na.strings = "", stringsAsFactors = FALSE)
+metadata <- read.csv("data/metadata_gender.csv", na.strings = "", stringsAsFactors = FALSE)
+
+metadata$name1 <- sapply(sapply(metadata$name,strsplit, split = " ", perl = TRUE), '[', 1)
+metadata$name1 <- sapply(metadata$name1, tolower)
+metadata$name2 <- sapply(sapply(metadata$name,strsplit, split = " ", perl = TRUE), '[', 2)
+metadata$name2 <- sapply(metadata$name2, tolower)
+metadata$gender1 <- sapply(lapply(metadata$name1, gender), '[[', 4)
+metadata$gender2 <- sapply(lapply(metadata$name2, gender), '[[', 4)
+write.csv(metadata, file = "data/metadata_gender.csv", row.names = FALSE)
+
+##########################################################################
+#Statistical Analysis
+##########################################################################
+
+metadata_uk <- metadata[metadata$timezone == 'London' & (metadata$lang == 'en' | metadata$lang == 'en-gb' | metadata$lang == 'en-GB') 
+                        & metadata$protected == 'False' & metadata$location != 'NA' & (metadata$gender1 == 'male' | metadata$gender1 == 'female')
+                        & !(metadata$snp == 1 & metadata$ukip == 0 & metadata$libdem == 0 & metadata$cons == 0 & metadata$lab == 0 & metadata$bnp == 0),]
+metadata_ukip <- metadata_uk[metadata_uk$ukip == 1,]
+write.csv(metadata_uk, file = "data/metadata_uk.csv", row.names = FALSE)
+write.csv(metadata_ukip, file = "data/metadata_ukip.csv", row.names = FALSE)
+
+metadata_uk[,c(2:8, 20)] <- data.frame(apply(metadata_uk[,c(2:8, 20)], 2, as.factor))
+
+glm.1 <- glm(ukip ~ gender1 + bnp + lab + cons + libdem, data = metadata_uk, family = binomial())
+glm.2 <- glm(bnp ~ gender1 + ukip + lab + cons + libdem, data = metadata_uk, family = binomial())
+summary(glm.1)
+summary(glm.2)
